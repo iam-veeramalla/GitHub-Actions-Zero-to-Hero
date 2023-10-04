16a4794fbf5f9eb8ca8066dd91c414a8a88733d4
LR2709 <146945884+LR2709@users.noreply.github.com>
2023-10-04 16:54:40 +0530

    basic operations.py
    Includes addition and multiplication

--- src/addition.py
+++ src/addition.py
@@ -6,3 +6,10 @@ def add(a, b):
 def test_add():
     assert add(1, 2) == 3
     assert add(1, -1) == 0
+
+def multiply(a,b):
+    return a*b
+
+def test_multiplication(a,b):
+    assert multiply(1,2) == 2
+    assert multiply(10,20) == 200
