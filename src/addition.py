13766593b34a858c5c54b4e90018524d1121fcfe
VenkataViswas <123818703+VenkataViswas@users.noreply.github.com>
2024-02-08 18:54:17 +0530

    Update addition.py

--- src/addition.py
+++ src/addition.py
@@ -1,8 +1,20 @@
-# app.py
-# This is a test commit
+'''
+This is a simple program to understand the basics of github actions
+'''
 def add(a, b):
     return a + b
 
+def sub(a,b):
+    return a-b
+
+def mult(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
 def test_add():
     assert add(1, 2) == 3
-    assert add(1, -1) == 0
+    assert sub(1, -1) == 2
+    assert mult(5,5) == 25
+    assert div(-1,-1) == 1
